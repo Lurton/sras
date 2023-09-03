0f1c6f65ab2db71751fb8daa6c198cce7af791db
Argus Ndabashinze <argus.ndabashinze@thegocompany.co.za>
2023-09-03 22:28:32 +0200

    Updates

--- scripts/populate_data.py
+++ scripts/populate_data.py
@@ -5,7 +5,6 @@ import django
 
 from pathlib import Path
 
-
 BASE_PATH = Path(__file__).resolve(strict=True).parent.parent
 sys.path.append(f"{BASE_PATH}")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sras.settings")
@@ -14,10 +13,10 @@ django.setup()
 
 
 def create_campus_residence_rooms(campus_count: int, res_count: int, rooms_count):
-    from administration.models import Campus
+    from administration.models import Campus, Residence, Room
 
-    for i in campus_count:
-        Campus.objects.create(
+    for i in range(campus_count):
+        campus, created = Campus.objects.get_or_create(
           name=f"Campus_{i}",
           location=f"Location_{i}",
           address=f"Address_{i}",
@@ -25,9 +24,44 @@ def create_campus_residence_rooms(campus_count: int, res_count: int, rooms_count
           email_address=f"campus_{i}@cput.ac.za"
         )
 
+        for res in range(res_count):
+            residence, created = Residence.objects.get_or_create(
+                name=f"Campus_{i}_Residence_{res}",
+                campus=campus
+            )
+
+            for room in range(rooms_count):
+                room, created = Room.objects.get_or_create(
+                    number=f"R{res}-{room}",
+                    floor=res,
+                    residence=residence
+                )
+
+
+def delete_all():
+    from administration.models import Campus, Residence, Room
+
+    campuses = Campus.objects.all()
+    residences = Residence.objects.all()
+    rooms = Room.objects.all()
+
+    for each in campuses:
+        each.delete()
+
+    for each in residences:
+        each.delete()
+
+    for each in rooms:
+        each.delete()
+
 
 def main():
+    campus_amount = 5
+    res_amount = 12
+    room_amount = 20
 
+    create_campus_residence_rooms(campus_amount, res_amount, room_amount)
+    # delete_all()
     print("Done!")
 
 
